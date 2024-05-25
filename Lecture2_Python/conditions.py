868bd7f826793f4f9bc2187e5bbbe63b53de1995
PhucDT <dinhthaiphuc463@gmail.com>
2024-05-25 13:23:16 +0700

    Lecture2_Python

---
+++ Lecture2_Python/conditions.py
@@ -0,0 +1,8 @@
+n = int(input("Number: "))
+
+if n < 0:
+    print(f"{n} be hon 0")
+elif n > 0:
+    print(f"{n} lon hon 0")
+else:
+    print(f"{n} bang 0")
---
+++ Lecture2_Python/variables.py
@@ -0,0 +1,6 @@
+a = 28
+b = 1.5
+c = "Hello!"
+d = True
+e = None
+print(a)
